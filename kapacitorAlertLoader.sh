0c98d25501c9a9f3c26465160a6083985ba29500
Jeffries <qnqr@M00973461.nordstrom.net>
2017-03-13 10:29:08 -0700

    delete tasks with pattern

--- kapacitorAlertLoader.sh
+++ kapacitorAlertLoader.sh
@@ -20,9 +20,13 @@ defineTemplates() {
   done;
 }
 
-deleteTasks() { handleError $(kapacitor delete tasks "$1" 2>&1 1> /dev/null) }
+deleteTasks() {
+  handleError $(kapacitor delete tasks "$1" 2>&1 1> /dev/null)
+}
 
-deleteTemplates() { handleError $(kapacitor delete templates "$1" 2>&1 1> /dev/null) }
+deleteTemplates() {
+  handleError $(kapacitor delete templates "$1" 2>&1 1> /dev/null)
+}
 
 enableTasks() {
   task=${1%\.*}
@@ -39,9 +43,13 @@ reloadTasks() {
   handleError $(kapacitor reload "$task" 2>&1 >/dev/null)
 }
 
-getType() { if grep -q "stream" "$1"; then type="stream"; else type="batch"; fi; echo $type }
+getType() {
+  if grep -q "stream" "$1"; then type="stream"; else type="batch"; fi; echo $type
+}
 
-handleError() { if [[ "$1" == *"err"* || "$1" == *"failed"* || "$1" == *"unknown"* ]]; then exit 1; fi }
+handleError() {
+  if [[ "$1" == *"err"* || "$1" == *"failed"* || "$1" == *"unknown"* ]]; then exit 1; fi
+}
 
 set -x #echo on
 [ -z "$FUNCTION" ] && echo "Need to set FUNCTION for kapacitorAlertLoader to execute. Ex. defineTasks, defineTemplates, defineTasksFromTemplates, enableTasks, disableTasks, or reloadTasks" && exit 1;
@@ -51,9 +59,9 @@ if [[ $FUNCTION = "defineTemplates" ]]; then
 elif [[ $FUNCTION = "enableTasks" ]]; then
   enableTasks $ALERT
 elif [[ $FUNCTION = "deleteTasks" ]]; then
-  deleteTasks $ALERT
+  deleteTasks "$ALERT"
 elif [[ $FUNCTION = "deleteTemplates" ]]; then
-  deleteTemplates $ALERT
+  deleteTemplates "$ALERT"
 elif [[ $FUNCTION = "disableTasks" ]]; then
   disableTasks $ALERT
 else
