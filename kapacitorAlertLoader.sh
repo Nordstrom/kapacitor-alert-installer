cf05c7347d39400125b7343c35001ef4cca28da0
Jeffries <qnqr@M00973461.nordstrom.net>
2017-03-13 09:41:23 -0700

    delete

--- kapacitorAlertLoader.sh
+++ kapacitorAlertLoader.sh
@@ -20,6 +20,10 @@ defineTemplates() {
   done;
 }
 
+deleteTasks() { handleError $(kapacitor delete tasks "$1" 2>&1 1> /dev/null) }
+
+deleteTemplates() { handleError $(kapacitor delete templates "$1" 2>&1 1> /dev/null) }
+
 enableTasks() {
   task=${1%\.*}
   handleError $(kapacitor enable "$task" 2>&1 >/dev/null)
@@ -35,34 +39,28 @@ reloadTasks() {
   handleError $(kapacitor reload "$task" 2>&1 >/dev/null)
 }
 
-getType() {
-  if grep -q "stream" "$1"; then type="stream"; else type="batch"; fi; echo $type
-}
+getType() { if grep -q "stream" "$1"; then type="stream"; else type="batch"; fi; echo $type }
 
-handleError() {
-  if [[ "$1" == *"err"* || "$1" == *"failed"* || "$1" == *"unknown"* ]]; then exit 1; fi
-}
-
+handleError() { if [[ "$1" == *"err"* || "$1" == *"failed"* || "$1" == *"unknown"* ]]; then exit 1; fi }
 
 set -x #echo on
 [ -z "$FUNCTION" ] && echo "Need to set FUNCTION for kapacitorAlertLoader to execute. Ex. defineTasks, defineTemplates, defineTasksFromTemplates, enableTasks, disableTasks, or reloadTasks" && exit 1;
 [ -z "$ALERT" ] && echo "Need to set ALERT. Ex. scriptname.tick, prefix*, or *suffix.tick" && exit 1;
-# defineTempaltes
 if [[ $FUNCTION = "defineTemplates" ]]; then
   defineTemplates $ALERT
-# enableTasks
 elif [[ $FUNCTION = "enableTasks" ]]; then
   enableTasks $ALERT
-# disableTasks
+elif [[ $FUNCTION = "deleteTasks" ]]; then
+  deleteTasks $ALERT
+elif [[ $FUNCTION = "deleteTemplates" ]]; then
+  deleteTemplates $ALERT
 elif [[ $FUNCTION = "disableTasks" ]]; then
   disableTasks $ALERT
 else
   [ -z "$DATABASE" ] && echo "Need to set DATABASE alert will target" && exit 1;
   [ -z "$RETENTION_POLICY" ] && echo "Need to set RETENTION_POLICY alert will have" && exit 1;
-  # defineTasks
   if [[ $FUNCTION = "defineTasks" ]]; then
     defineTasks $ALERT
-  # defineTasksFromTemplates
   elif [[ $FUNCTION = "defineTasksFromTemplates" ]]; then
     [ -z "$VARS" ] && echo "When deploying alert that is based on a template, you need to set a VARS json file. Ex: vars.json" && exit 1;
     defineTasksFromTemplates $ALERT
